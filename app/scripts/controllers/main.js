8d7cb5160e0fd432dc61ee3933ac9d8c6ccf83b3
bborgesr <barb.b.ribeiro@gmail.com>
2015-11-19 12:42:51 +0000

    improved sorting algorithm

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -32,7 +32,34 @@ angular.module('ccApp')
   					});
             
   					// sort careers by career capital (decending order)
-  					$scope.careers = _.sortBy($scope.careers, function(career){ return (0 - career.custom_fields.careerCapital); });
+  					$scope.careers.sort(function(a, b) {
+              if (a.custom_fields.careerCapital < b.custom_fields.careerCapital) {
+                return 1;
+              }
+              if (a.custom_fields.careerCapital > b.custom_fields.careerCapital) {
+                return -1;
+              }
+              if (a.custom_fields.careerCapital == b.custom_fields.careerCapital) {
+                if ($scope.entries[0].value == 4) { // if max math 
+                  if (a.custom_fields.requiresQuantitativeSkills > b.custom_fields.requiresQuantitativeSkills) {
+                    return -1;
+                  }
+                  if (a.custom_fields.requiresQuantitativeSkills < b.custom_fields.requiresQuantitativeSkills) {
+                    return 1;
+                  }
+                }
+                if ($scope.entries[1].value == 4) {  // if max writing
+                  if (a.custom_fields.requiresVerbalAndSocialSkills > b.custom_fields.requiresVerbalAndSocialSkills) {
+                    return -1;
+                  }
+                  if (a.custom_fields.requiresVerbalAndSocialSkills < b.custom_fields.requiresVerbalAndSocialSkills) {
+                    return 1;
+                  }
+                }
+              }
+              // a must be equal to b
+              return 0;
+            });
   	    	}
         }
       };
